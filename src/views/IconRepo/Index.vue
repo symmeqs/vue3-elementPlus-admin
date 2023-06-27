582d1d3a17100e1ec27994590db8a7657e714d56
15521071030 <zhaoxingjun2018@gmail.com>
2023-06-27 23:05:37 +0800

    增加 IconRepo

--- src/views/IconRepo/Index.vue
+++ src/views/IconRepo/Index.vue
@@ -1,12 +1,30 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { iconList } from './iconRepo'
+
+// todo remove
+iconList.push(...iconList)
+</script>
 
 <template>
-  <div>
-    <div class="">
-      搜索框
-    </div>
+  <div class="bg-white w-full h-full rounded-tl-md p-5">
     <div>
-      图标集合
+      <div
+        class="w-[80%] grid grid-cols-3 lg-md:grid-cols-10 overflow-hidden
+              border-t-solid border-l-solid border-1 border-gray-300 rounded-md text-gray-500"
+      >
+        <div
+          v-for="item in iconList" :key="item.icon"
+          class="h-120px flex items-center hover:(bg-gray-1 cursor-pointer)
+                border-r-solid border-b-solid border-1 border-gray-300"
+        >
+          <div class="mx-auto overflow-hidden">
+            <div :class="item.icon" class="w-26px h-26px mx-auto" />
+            <div class="text-sm line-height-12 w-[100%] whitespace-nowrap overflow-hidden text-ellipsis">
+              {{ item.name }}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
---
+++ src/views/IconRepo/iconRepo.ts
@@ -0,0 +1,45 @@
+export const iconList = [
+  {
+    icon: 'i-ant-design-dashboard-filled',
+    name: 'dashboard',
+  },
+  {
+    icon: 'i-clarity-document-solid',
+    name: 'document',
+  },
+  {
+    icon: 'i-cib-telegram-plane',
+    name: 'telegram-plane',
+  },
+  {
+    icon: 'i-heroicons-cog-6-tooth-solid',
+    name: 'cog-6-tooth',
+  },
+  {
+    icon: 'i-cib-nintendo-gamecube',
+    name: 'gamecube',
+  },
+  {
+    icon: 'i-emojione-monotone-sun',
+    name: 'monotone-sun',
+  },
+  {
+    icon: 'i-emojione-monotone-crescent-moon',
+    name: 'crescent-moon',
+  }, {
+    icon: 'i-ep-expand',
+    name: 'dashboard',
+  },
+  {
+    icon: 'i-ep-fold',
+    name: 'document',
+  },
+  {
+    icon: 'i-ep-full-screen',
+    name: 'telegram-plane',
+  },
+  {
+    icon: 'i-ep-bell',
+    name: 'cog-6-tooth',
+  },
+]
